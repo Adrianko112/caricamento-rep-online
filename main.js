139be2d422270d3217ad464ea46c4c25441f52c7
Adriano Germinal <adrianogerminal@gmail.com>
2026-03-02 14:07:53 +0100

    esercizio variabili js

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 
+let num1 = 10;
+
+let num2 = 20;
+
+console.log(num1);
+
+console.log(num2);
+
+let stringa = 'ciao mi chiamo Adriano';
+
+console.log(stringa);
+
+stringa = 'ciao mi chiamo Michele';
+
+console.log(stringa);
+
+const PIGRECO =  3.14159265359;
+
+console.log(PIGRECO);
+
+PIGRECO = 1200;
+
+console.log(PIGRECO);
+
+
